e333f1fb2b396ab026485abea7327c0f72ac28e5
Johann <jherre848@west-mec.org>
2023-10-10 13:54:12 -0700

    componants

---
+++ frontend/src/components/StudentList.js
@@ -0,0 +1,72 @@
+import React, { useState, userEffect} from "react";
+import {link} from "react-router-dom";
+import axios from "axios";
+
+function StudentList() {
+    const [userForm, setUserForm] = useState([]);
+
+    const deleteStudent = (_id) => {
+        axios
+        .delete("http://localhost:4000/students/delete-student/" + _id)
+        .then(() => {
+            console.log("Data successfully delted!");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+
+    useEffect(() => {
+        axios
+        .get("http://localhost:4000/students/")
+        .then((res) => {
+            setUserForm(res.data.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }, [userForm]);
+
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Roll no</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userForm.map((user, index) => {
+                        return (
+                            <tr key={index}>
+                                <th scope="row">{user._id}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>@{user.rollno}</td>
+                                <td>
+                                    <link
+                                    className="btn btn-primary btn-sm me-2"
+                                    to={"/edit-student/" + user._id}
+                                    >
+                                        Edit
+                                    </link>
+                                    <button className="btn btn-danger btn-sm"
+                                    onClick={() => deleteStudent(user._id)}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default StudentList;
