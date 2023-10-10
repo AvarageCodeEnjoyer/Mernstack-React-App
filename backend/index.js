7a8a9227e6a68498cdaea6572adb0429c2a87495
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-10-10 13:02:08 -0700

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const studentRoute = require('./routes/student.routes')
+
+const port = process.env.port || 4000
+
+mongoose
+  .connect("mongodb+srv://ndross427:<password>@cluster0.ipo5a6z.mongodb.net/?retryWrites=true&w=majority")
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err.reason);
+  })
+
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended : true
+}))
+app.use(cors())
+app.use("/students", studentRoute)
+app.use((req, res, next) => {
+  next(createError(404))
+})
+app.use((err, req, res, next) => {
+  if(!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message)
+})
+
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`)
+})
+
+
---
+++ backend/routes/student.route.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const router = express.router()
+
+
+let studentSchema = require("../models/Student")
+
+router.route("/create-student").post(async (req, res, next) => {
+  await studentSchema
+    .create(req.body)
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "Data successfully uploaded",
+        status: 200,
+      })
+    })
+    .catch(err => {
+      return next(err)
+    })
+})
+
+
+router.route("/").get(async (req, res, next) => {
+  await studentSchema
+    .find()
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "All items successfully fetched",
+        status: 200,
+      })
+    })
+    .catch(err => {
+      return next(err)
+    })
+})
+
+
+router.route("/get-student/:id").get(async (req, res, next) => {
+  await studentSchema
+    .findById(req.params.id)
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "All items successfully fetched",
+        status: 200,
+      })
+    })
+    .catch(err => {
+      return next(err)
+    })
+})
+
+router.route("/update-student/:id").put(async (req, res, next) => {
+  await studentSchema
+    .findByIdAndUpdate(req.params.id)
+    .then(() => {
+      res.json({
+        message: "Data successfully updated",
+      })
+    })
+    .catch(err => {
+      return next(err)
+    })
+})
+
+router.route("/delete-student/:id").delete(async (req, res, next) => {
+  await studentSchema
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.json({
+        message: "Data successfully updated",
+      })
+    })
+    .catch(err => {
+      return next(err)
+    })
+})
+
+module.exports = router
+
