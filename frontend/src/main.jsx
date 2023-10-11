b31d6a8a8f2e18425bf7e9371d15c79bd6be4528
NikoDrossos <ndross427@West-MEC.org>
2023-10-11 12:48:26 -0700

    jsx files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDom from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import "index.css"
+import App from "./App"
+// import reportWebVitals from "./reportWebVitals"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDom.createRoot(document.getElementById("root"))
+root.render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>
 )
+
+// reportWebVitals()
